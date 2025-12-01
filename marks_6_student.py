38f7fdb76e6d306485736330a890e86ac4612382
shreyaa1025 <shreyu1320@gmail.com>
2025-12-01 22:02:30 +0530

    marks input collection for 7 students and sort

---
+++ marks_6_student.py
@@ -0,0 +1,18 @@
+marks = []
+
+f1 = int(input("enter marks:"))
+marks.append(f1)
+f2 = int(input("enter marks:"))
+marks.append(f2)
+f3 = int(input("enter marks:")) 
+marks.append(f3)
+f4 = int(input("enter marks:"))
+marks.append(f4)
+f5 = int(input("enter marks:"))
+marks.append(f5)
+f6 =int(input("enter marks:"))
+marks.append(f6)
+f7 =int(input("enter marks:"))
+marks.append(f7)
+
+print(marks)
