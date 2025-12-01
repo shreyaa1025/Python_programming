dabd4cd6795471765851ead8a2ac18811c354675
shreyaa1025 <shreyu1320@gmail.com>
2025-12-01 21:43:17 +0530

    list methods (remove , reverse , append , insert)

---
+++ list_methods.py
@@ -0,0 +1,14 @@
+friends = ["apple","orange",34,37.045,"shreya","anil"]
+print(friends)
+
+friends.append("rashmi")
+print(friends)
+
+friends.remove(34)
+print(friends)
+
+friends.insert(3,7)
+print(friends)
+
+friends.reverse()
+print(friends)
