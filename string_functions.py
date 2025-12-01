c21e1b6914d3efd93e045b3bf1d50ed543cedee4
shreyaa1025 <shreyu1320@gmail.com>
2025-12-01 20:32:32 +0530

    string functions ( len , endswith ,startswith ,capitalize , find , replace)

---
+++ string_functions.py
@@ -0,0 +1,7 @@
+name = "shreya is a bad girl"
+print(len(name))
+print(name.endswith("ya"))
+print(name.startswith("sh"))
+print(name.capitalize())
+print(name.find("y"))
+print(name.replace("bad","good"))
