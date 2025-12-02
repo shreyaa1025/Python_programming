da58cdae4f8e148aaba41a04549a774c6f7f6ef8
shreyaa1025 <shreyu1320@gmail.com>
2025-12-02 22:30:18 +0530

    store names and language in string
    Collects names and their corresponding languages from user input and stores them in a dictionary.

---
+++ names_lang.py
@@ -0,0 +1,19 @@
+s = {}
+name = input("enter the names:")
+lang = input("enter the language:")
+s.update({name:lang})
+
+name = input("enter the names:")
+lang = input("enter the language:")
+s.update({name:lang})
+
+name = input("enter the names:")
+lang = input("enter the language:")
+s.update({name:lang})
+
+name = input("enter the names:")
+lang = input("enter the language:")
+s.update({name:lang})
+
+
+print(s)
