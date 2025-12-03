b25a508aa2e305cc195cb79bb6e1381b538fd61e
shreyaa1025 <shreyu1320@gmail.com>
2025-12-03 20:18:20 +0530

    age validation with if-elif-else structure

---
+++ if_elif_else.py
@@ -0,0 +1,11 @@
+a = int(input("enter you age:"))
+
+if(a>=18):
+    print("you are above consent")
+    print("good for you")
+    
+elif(a<=0):
+    print("you have entered invalid age")
+else:
+    print("you are below consent")
+    
