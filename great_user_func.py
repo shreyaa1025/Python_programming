ce52521c36a3d34381d1648b075a2f2823c0450b
shreyaa1025 <shreyu1320@gmail.com>
2025-12-14 16:16:40 +0530

    Add function to greet users with their names

---
+++ great_user_func.py
@@ -0,0 +1,10 @@
+# n = input("enter a name:")
+
+def a():
+    n = input("enter a name:")
+    c = input("enter a name:")
+    b = input("enter a name:")
+    print( n,c,b , "welcome!")
+    
+
+a()
